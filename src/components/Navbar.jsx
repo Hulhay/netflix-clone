501f67e9cfba291394d9d2bdcf8180546770556b
Prasetyo <prasetyosyifa@gmail.com>
2022-11-05 02:11:45 +0700

    cleaning code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ page }) => {
-  const [navIcon, setNavIcon] = useState(page);
-
   return (
-    <div className="bg-chinese-black w-full h-14 absolute bottom-0">
+    <div className="bg-chinese-black w-full h-14 fixed bottom-0">
       <div className="flex justify-evenly items-center">
         <Link to={'/'}>
           <button className="h-14 w-6">
@@ -21,7 +18,7 @@ const Navbar = ({ page }) => {
                 clipRule="evenodd"
                 d="M29.4047 0.899316L28.1984 2.83718e-05L27 0.909915L-6.91414e-06 21.4099L2.41883 24.5957L7.79042 20.5173L9.71481 46.6497L9.85128 48.5028H11.7094H44.2094H45.9437L46.1893 46.7859L49.8575 21.135L54.5141 24.6063L56.9047 21.3993L29.4047 0.899316ZM46.2061 18.4131L28.2204 5.00559L11.5889 17.6333L13.5675 44.5028H42.4751L46.2061 18.4131Z"
                 fill="white"
-                opacity={navIcon === 'home' ? '1' : '0.5'}
+                opacity={page === 'home' ? '1' : '0.5'}
               />
             </svg>
           </button>
@@ -39,7 +36,7 @@ const Navbar = ({ page }) => {
               clipRule="evenodd"
               d="M41 22C41 32.4934 32.4934 41 22 41C11.5066 41 3 32.4934 3 22C3 11.5066 11.5066 3 22 3C32.4934 3 41 11.5066 41 22ZM35.5246 39.3531C31.7937 42.265 27.0995 44 22 44C9.84974 44 0 34.1503 0 22C0 9.84974 9.84974 0 22 0C34.1503 0 44 9.84974 44 22C44 27.6099 41.9003 32.7294 38.4438 36.6154L48.4142 46.5858L45.5858 49.4142L35.5246 39.3531Z"
               fill="white"
-              opacity={navIcon === 'search' ? '1' : '0.5'}
+              opacity={page === 'search' ? '1' : '0.5'}
             />
           </svg>
         </button>
@@ -59,18 +56,18 @@ const Navbar = ({ page }) => {
               rx="3.5"
               stroke="white"
               strokeWidth="5"
-              opacity={navIcon === 'comingSoon' ? '1' : '0.5'}
+              opacity={page === 'comingSoon' ? '1' : '0.5'}
             />
             <path
               d="M39 14L37.3003 5.84155C36.8559 3.70811 34.7854 2.32304 32.6438 2.72653L6.02151 7.74232C3.81548 8.15795 2.38335 10.3072 2.84916 12.5032L8.32758 38.33C8.71961 40.1782 10.3513 41.5 12.2405 41.5H13.5"
               stroke="white"
               strokeWidth="4"
-              opacity={navIcon === 'comingSoon' ? '1' : '0.5'}
+              opacity={page === 'comingSoon' ? '1' : '0.5'}
             />
             <path
               d="M29 42.7839V28.6333C29 28.1448 29.5412 27.8505 29.9513 28.1161L41.6159 35.6703C42.2461 36.0784 42.2185 37.0095 41.5653 37.3797L30.493 43.654C29.8264 44.0317 29 43.5502 29 42.7839Z"
               fill="white"
-              opacity={navIcon === 'comingSoon' ? '1' : '0.5'}
+              opacity={page === 'comingSoon' ? '1' : '0.5'}
             />
           </svg>
         </button>
@@ -87,14 +84,14 @@ const Navbar = ({ page }) => {
                 d="M22 0V43M22 43L8.5 32.5M22 43L35 32.5"
                 stroke="white"
                 strokeWidth="5"
-                opacity={navIcon === 'download' ? '1' : '0.5'}
+                opacity={page === 'download' ? '1' : '0.5'}
               />
               <rect
                 y="49"
                 width="44"
                 height="5"
                 fill="white"
-                opacity={navIcon === 'download' ? '1' : '0.5'}
+                opacity={page === 'download' ? '1' : '0.5'}
               />
             </svg>
           </button>
@@ -111,21 +108,21 @@ const Navbar = ({ page }) => {
               width="50"
               height="5"
               fill="white"
-              opacity={navIcon === 'more' ? '1' : '0.5'}
+              opacity={page === 'more' ? '1' : '0.5'}
             />
             <rect
               y="17"
               width="50"
               height="5"
               fill="white"
-              opacity={navIcon === 'more' ? '1' : '0.5'}
+              opacity={page === 'more' ? '1' : '0.5'}
             />
             <rect
               y="33"
               width="50"
               height="5"
               fill="white"
-              opacity={navIcon === 'more' ? '1' : '0.5'}
+              opacity={page === 'more' ? '1' : '0.5'}
             />
           </svg>
         </button>

f9cc8d9bd037030d4f8ca3e0c7002d690b7becb0
Prasetyo <prasetyosyifa@gmail.com>
2022-11-04 17:19:54 +0700

    tweak code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MovieBanner from '../components/MovieBanner';
 import Toolbar from '../components/Toolbar';
 import Navbar from '../components/Navbar';
-import CategoryMovie from '../components/CategoryMovie';
 
 const Home = () => {
   return (

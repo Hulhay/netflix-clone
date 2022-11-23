dd4868165abc3cd88c051479524f59672a713a32
Prasetyo <prasetyosyifa@gmail.com>
2022-11-23 22:12:49 +0700

    fix search bar input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,11 +7,11 @@ const SearchBar = ({ setIsSearch }) => {
   return (
     <div className="p-4">
       <div className="flex items-center justify-between h-12 w-full bg-dark-charcoal p-4 rounded-md">
-        <div className="flex justify-center items-center ">
+        <div className="flex justify-start items-center w-full">
           <FiSearch className="text-white text-xl opacity-70 h-5 w-5 mr-3" />
           <input
             type="text"
-            className="outline-none bg-dark-charcoal text-white"
+            className="outline-none bg-dark-charcoal text-white w-full pr-4"
             placeholder="Search for something"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
